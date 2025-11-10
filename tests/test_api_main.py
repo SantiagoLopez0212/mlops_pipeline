17d3afc43084c41018bbfa8e5c8519b657b712c4
SantiagoLopez <108092570+SantiagoLopez0212@users.noreply.github.com>
2025-11-10 15:30:38 -0500

    Update test_api_main.py

--- tests/test_api_main.py
+++ tests/test_api_main.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Asegura que se pueda importar src/
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from fastapi.testclient import TestClient
 from src.api_main import app
 
